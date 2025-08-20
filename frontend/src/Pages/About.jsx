97ea956707168542da5253d447de433340fae9f5
farhanbashir3354 <farhanbashir3354@gmail.com>
2025-08-20 11:28:14 +0500

    revised 3

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -1,11 +1,11 @@
-import React from "react";
 import Details from "../component/about/details";
 import StatsSection from "../component/about/Stats";
-export default function About() {
-  return (
-    <>
-      <Details />
-      <StatsSection />
-    </>
-  );
+const About =()=>{
+ return(
+  <>
+    <Details/>
+    <StatsSection/>
+  </>
+ )
 }
+export default About;
