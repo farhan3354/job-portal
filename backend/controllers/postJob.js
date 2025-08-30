db68102b293a266aa8f1fc93d06f0c33020486f3
farhanbashir3354 <farhanbashir3354@gmail.com>
2025-08-30 19:25:31 +0500

    edit and get api of post jobs

--- backend/controllers/postJob.js
+++ backend/controllers/postJob.js
@@ -59,3 +59,52 @@ export const createJob = async (req, res) => {
       .json({ message: "Server error", error: error.message });
   }
 };
+
+export const fetchjobs = async (req, res) => {
+  try {
+    const jobs = await Job.find().populate("postedBy", "name email");
+    if (!jobs) {
+      return res.status(404).json({ message: "Job not found" });
+    }
+    return res.status(200).json({ message: "Data fetch successfully", jobs });
+  } catch (error) {
+    return res.status(500).json({ message: "Error came in the server", error });
+  }
+};
+
+export const fetchidjobs = async (req, res) => {
+  try {
+    const { id } = req.params.id;
+    if (!id) {
+      return res.status(404).json({ message: "Id is required" });
+    }
+    const jobs = await Job.findById(id).populate("postedBy", "name email");
+    if (!jobs) {
+      return res.status(404).json({ message: "Job not found" });
+    }
+    res.status(200).json({ message: "Data fetch successfully", jobs });
+  } catch (error) {
+    res.status(500).json({ message: "Error came in the server", error });
+  }
+};
+
+export const editjob = async (req, res) => {
+  try {
+    const changejobdetail = await Job.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!changejobdetail) {
+      return res.status(404).json({ message: "Job not found in the database" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "Job updated successfully", changejobdetail });
+  } catch (error) {
+    console.log(`Error in editjob API:`, error);
+    res.status(500).json({ message: "Server error updating job" });
+  }
+};
