c6f90da077ec10a7685d1c4b4db1fe468f2d45f0
farhan3354 <farhanbashir3354@gmail.com>
2025-10-09 20:04:33 +0500

    Fix import case for AdminSideBar

--- frontend/src/component/common/admin/AdminLayout.jsx
+++ frontend/src/component/common/admin/AdminLayout.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import AdminHeader from "./AdminHeader";
-import AdminSidebar from "./AdminSideBar";
+import AdminSideBar from "./AdminSideBar";
 
 export default function AdminLayout() {
   return (
     <div className="flex flex-col min-h-screen">
       <AdminHeader />
       <div className="flex flex-1">
-        <AdminSidebar />
+        <AdminSideBar />
         <main className="flex-1 p-6 bg-gray-50">
           <Outlet />
         </main>
